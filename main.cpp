b74f765fde4483dc9204651dde870b67eec8346f
RomitoGiovanni <g.romito15@studenti.uniba.it>
2024-12-06 13:00:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,37 @@ int main() {
 
     Dizionario<std::string> dict;
 
-    dict.inserisci("apple", "fruit");
-    dict.inserisci("car", "vehicle");
-    dict.inserisci("book", "object");
+    // Inserimento di chiavi e valori
+    dict.inserisci("zebra", "animale");
+    dict.inserisci("capra", "animale");
+    dict.inserisci("mosca", "insetto");
+    dict.inserisci("leone", "animale");
 
-    std::cout << "Value for 'apple': " << dict.recupera("apple") << "\n";
-    std::cout << "Value for 'car': " << dict.recupera("car") << "\n";
-    std::cout << "Value for 'pen': " << dict.recupera("pen") << "\n";
+    dict.inserisci("abcde", "parola");  // Crea collisione con "zebra" perche danno lo stesso hash
 
+    // Stampa dizionario
+    std::cout << "\ndizionario:\n";
     dict.stampa();
 
-    return 0;
+    // Recupera un valore
+    std::cout << "\nValore per 'zebra': " << dict.recupera("zebra") << "\n";
+    std::cout << "Valore per 'gorilla': " << dict.recupera("gorilla") << "\n"; // Chiave non presente
+
+    // Verifica se una chiave esiste
+    if (dict.appartiene("capra")) {
+    std::cout << "\n'capra' appartiene\n";
+    } else {
+    std::cout << "\n'capra' non appertiene\n";
+    }
+
+    if (dict.appartiene("leone")) {
+    std::cout << "'leone' appartiene\n";
+    } else {
+    std::cout << "'leone' non appartiene\n";
+    }
+
+    // Cancellazione di una chiave
+    std::cout << "\nCancellazione di 'zebra':\n";
+    dict.cancella("zebra");
+
 }
